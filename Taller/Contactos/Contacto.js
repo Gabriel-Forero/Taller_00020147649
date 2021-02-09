201f2d8268c24706686e1789e62abc0a4549119f
Gabriel-Forero <69248583+Gabriel-Forero@users.noreply.github.com>
2021-02-09 16:05:11 -0500

    Add files via upload

---
+++ Taller/Contactos/Contacto.js
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <script>
+
+        </script>
+        
+    </head>
+
+    <body>
+       <script>
+        function Alerta() {
+            alert("Gracias por llenar el formulario")
+            
+        }
+       </script>
+        
+
+    </body>
+</html>
